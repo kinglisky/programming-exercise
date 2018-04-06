1f3010f3d55be674ae8bfd8b415cabbfe4282e32
芊野 <free.nan.sky@gmail.com>
2018-04-07 00:07:11 +0800

    dynamicPlanning

--- 03-edition-distance.js
+++ 03-edition-distance.js
@@ -1,4 +1,7 @@
 // 最小编辑问题
+// https://github.com/youngwind/blog/issues/106
+// http://qinxuye.me/article/get-edit-distance-by-dynamic-programming/
+
 /**
  * 递归算法
  * @param {string} a
@@ -23,3 +26,49 @@ const a = 'time for'
 const b = 'for'
 console.log(`a: ${a}\nb: ${b}`)
 console.log('edition distance: ', recursion(a, b, a.length, b.length))
+
+/**
+ * 动态规划算法
+ * @param {string} a
+ * @param {string} b
+ * @returns {number} 从 a → b 的最小编辑距离
+ */
+function dynamicPlanning (a, b) {
+  let lenA = a.length
+  let lenB = b.length
+  let d = []
+  d[0] = []
+
+  for (let j = 0; j <= lenB; j++) {
+    d[0].push(j)
+  }
+
+  for (let i = 0; i <= lenA; i++) {
+    if (d[i]) {
+      d[i][0] = i
+    } else {
+      d[i] = []
+      d[i][0] = i
+    }
+  }
+
+  for (let i = 1; i <= lenA; i++) {
+    for (let j = 1; j <= lenB; j++) {
+      if (a[i - 1] === b[j - 1]) {
+        d[i][j] = d[i - 1][j - 1]
+      } else {
+        let m1 = d[i - 1][j] + 1
+        let m2 = d[i][j - 1] + 1
+        let m3 = d[i - 1][j - 1] + 1
+        d[i][j] = Math.min(m1, m2, m3)
+      }
+    }
+  }
+
+  return d[lenA][lenB]
+}
+
+const c = 'time for'
+const d = 'for'
+console.log(`c: ${c}\nd: ${d}`)
+console.log('edition distance: ', dynamicPlanning(c, d))
