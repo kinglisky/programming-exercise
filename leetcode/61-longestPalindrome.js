7808ea6b4c10d632e277330dbcd82fc55cb6146f
芊野 <free.nan.sky@gmail.com>
2020-09-28 22:42:40 +0800

    update

--- leetcode/61-longestPalindrome.js
+++ leetcode/61-longestPalindrome.js
@@ -3,24 +3,49 @@
  * @param {string} s
  * @return {string}
  */
- /**
+/**
+* @param {string} s
+* @return {string}
+*/
+// 扩展中心
+/*
+ * @lc app=leetcode id=5 lang=javascript
+ *
+ * [5] Longest Palindromic Substring
+ */
+/**
  * @param {string} s
  * @return {string}
  */
-// 扩展中心
-var longestPalindrome = function(s) {
-    let ans = '';
-     let n = s.length;
-     let dp = Array.from(new Array(n), () => new Array().fill(0));
-     for(let i = n-1; i >=0; i--) {
-         for(let j = i; j < n; j++) {
-             dp[i][j] = s[i] === s[j] && ( j - i < 2 || dp[i+1][j-1])
-             if(dp[i][j] && j - i + 1 > ans.length) {
-                 ans = s.substr(i,j - i + 1);
-             }
-         }
-     }
-     return ans;
- }
+var longestPalindrome = function (s) {
+    // babad
+    // tag : dp
+    if (!s || s.length === 0) return "";
+    let res = s[0];
+
+    const dp = [];
+
+    // 倒着遍历简化操作， 这么做的原因是dp[i][..]依赖于dp[i + 1][..]
+    for (let i = s.length - 1; i >= 0; i--) {
+        dp[i] = [];
+        for (let j = i; j < s.length; j++) {
+            if (j - i === 0) dp[i][j] = true;
+            // specail case 1
+            else if (j - i === 1 && s[i] === s[j]) dp[i][j] = true;
+            // specail case 2
+            else if (s[i] === s[j] && dp[i + 1][j - 1]) {
+                // state transition
+                dp[i][j] = true;
+            }
+
+            if (dp[i][j] && j - i + 1 > res.length) {
+                // update res
+                res = s.slice(i, j + 1);
+            }
+        }
+    }
+
+    return res;
+};
 
 console.log(longestPalindrome("babad"));
