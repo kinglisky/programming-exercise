336b6f41c55363c3891a5817378181303b978a62
芊野 <free.nan.sky@gmail.com>
2020-10-02 21:48:18 +0800

    feat: update

---
+++ leetcode/62-convert.js
@@ -0,0 +1,18 @@
+/**
+ * @param {string} s
+ * @param {number} numRows
+ * @return {string}
+ */
+var convert = function (s, numRows) {
+    if (numRows == 1 || s.length <= numRows)
+        return s;
+
+    const len = s.length;
+    const rows = [];
+    for (let i = 0; i < len; i++) {
+
+    }
+    return ans;
+};
+
+console.log(convert('LEETCODEISHIRING', 3));
