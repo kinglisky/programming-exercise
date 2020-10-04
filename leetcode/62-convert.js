2fa909bc9ea4b77d97597a3590342bed91545e3f
芊野 <free.nan.sky@gmail.com>
2020-10-04 09:38:06 +0800

    update

--- leetcode/62-convert.js
+++ leetcode/62-convert.js
@@ -3,14 +3,31 @@
  * @param {number} numRows
  * @return {string}
  */
+/**
+ * @param {string} s
+ * @param {number} numRows
+ * @return {string}
+ */
 var convert = function (s, numRows) {
-    if (numRows == 1 || s.length <= numRows)
+    if (numRows == 1)
         return s;
 
-    const len = s.length;
+    const len = Math.min(s.length, numRows);
     const rows = [];
-    for (let i = 0; i < len; i++) {
+    for (let i = 0; i < len; i++) rows[i] = "";
+    let loc = 0;
+    let down = false;
 
+    for (const c of s) {
+        rows[loc] += c;
+        if (loc == 0 || loc == numRows - 1)
+            down = !down;
+        loc += down ? 1 : -1;
+    }
+
+    let ans = "";
+    for (const row of rows) {
+        ans += row;
     }
     return ans;
 };
