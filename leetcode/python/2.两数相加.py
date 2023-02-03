327e402e52f44bf41dd626bb1a06a03579eb648b
chennan.wei <kinglisky@icloud.com>
2023-02-03 11:36:19 +0800

    feat: update

--- leetcode/python/2.两数相加.py
+++ leetcode/python/2.两数相加.py
@@ -6,17 +6,36 @@
 
 # @lc code=start
 # Definition for singly-linked list.
-# class ListNode(object):
-#     def __init__(self, val=0, next=None):
-#         self.val = val
-#         self.next = next
+class ListNode(object):
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+
 class Solution(object):
     def addTwoNumbers(self, l1, l2):
-        """
-        :type l1: ListNode
-        :type l2: ListNode
-        :rtype: ListNode
-        """
-        
-# @lc code=end
+        # 初始化链表
+        head = tree = ListNode()
 
+        val = tmp = 0
+        # 当三者有一个不为空时继续循环
+        while tmp or l1 or l2:
+            val = tmp
+            if l1:
+                val = l1.val + val
+                l1 = l1.next
+            if l2:
+                val = l2.val + val
+                l2 = l2.next
+
+            tmp = val // 10
+            val = val % 10
+
+            # 实现链表的连接
+            tree.next = ListNode(val)
+            tree = tree.next
+
+        return head.next
+
+
+# @lc code=end
